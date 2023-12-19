638df2ef5b2b467a2c2b132704ffc79918f37c08
Alexey <seleznev.trade@gmail.com>
2023-12-19 09:51:55 +0300

    10

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,19 +1,27 @@
 <script setup>
 import {ChartBarIcon, ClockIcon, ListBulletIcon} from "@heroicons/vue/24/outline";
 import NavItem from "@/components/NavItem.vue";
+import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from '@/constants'
+
+defineProps(['currentPage'])
+const emit = defineEmits(['navigate'])
 
 const navItems = {
-  timeline: ClockIcon,
-  activities: ListBulletIcon,
-  progress: ChartBarIcon
+  [PAGE_TIMELINE]: ClockIcon,
+  [PAGE_ACTIVITIES]: ListBulletIcon,
+  [PAGE_PROGRESS]: ChartBarIcon
 }
+
 </script>
 
 <template>
   <nav class="sticky bottom-0 z-10 bg-white">
     <ul class="flex items-center justify-around border-t">
-      <NavItem v-for="icon, page in navItems" :key="page"
-               :href="`#${page}`">
+      <NavItem v-for="(icon, page) in navItems"
+               :key="page"
+               :href="`#${page}`"
+               :class="{ 'bg-gray-200 pointer-events-none': page === currentPage}"
+                @click="emit('navigate', page)">
         <component :is="icon" class="h-6 w-6" /> {{page}}
       </NavItem>
     </ul>
