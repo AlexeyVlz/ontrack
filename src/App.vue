d9bd867963c13847f87dadab56736da9c9feed65
Alexey <seleznev.trade@gmail.com>
2024-01-03 14:34:06 +0300

    28

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,8 @@ import {generateTimelineItems, normalizePageHash} from './functions.js'
 const  currentPage = ref(normalizePageHash())
 const timelineItems = generateTimelineItems()
 
+const activities = ['Coding', 'Reading', 'Training']
+
 
 function goTo (page) {
   currentPage.value = page
@@ -25,7 +27,7 @@ function goTo (page) {
 
   <main class="flex flex-grow flex-col">
     <TheTimeline v-show="currentPage === PAGE_TIMELINE" :timeline-items="timelineItems"/>
-    <TheActivities v-show="currentPage === PAGE_ACTIVITIES"/>
+    <TheActivities v-show="currentPage === PAGE_ACTIVITIES" :activities="activities"/>
     <TheProgress v-show="currentPage === PAGE_PROGRESS"/>
   </main>
 
--- src/components/ActivityItem.vue
+++ src/components/ActivityItem.vue
@@ -3,28 +3,17 @@ import BaseSelect from "@/components/BaseSelect.vue";
 import BaseButton from "@/components/BaseButton.vue";
 import { TrashIcon } from "@heroicons/vue/24/outline";
 import {ref} from "vue";
+import { PERIOD_SELECT_OPTIONS } from "@/constants";
+import { isActivityValid } from "@/validators";
+
 
 defineProps({
   activity : {
     required: true,
-    type: String
+    type: String,
+    validator: isActivityValid
   }
 })
-
-const periodSelectOptions = [
-  {
-    value: 15,
-    label: '0:15'
-  },
-  {
-    value: 30,
-    label: '0:30'
-  },
-  {
-    value: 45,
-    label: '0:45'
-  }
-]
 
 const secondsToComplete = ref(null)
 
@@ -44,7 +33,7 @@ const secondsToComplete = ref(null)
           class="font-mono"
           placeholder="h:mm"
           :selected="secondsToComplete"
-          :options="periodSelectOptions"
+          :options="PERIOD_SELECT_OPTIONS"
           @select="secondsToComplete = $event"/>
     </div>
   </li>
