482fb53ae6293ce7ed4237f8eba284adf2b72f6a
Alexey <seleznev.trade@gmail.com>
2024-01-10 14:50:23 +0300

    34

--- src/pages/TheActivities.vue
+++ src/pages/TheActivities.vue
@@ -14,8 +14,11 @@ defineProps({
 })
 
 const emit = defineEmits({
-  deleteActivity: isActivityValid()
+  createActivity: isActivityValid,
+  deleteActivity: isActivityValid
 })
+
+let newActivity = ''
 
 
 </script>
@@ -30,8 +33,14 @@ const emit = defineEmits({
           @delete="emit('deleteActivity', activity)"
       />
     </ul>
-    <form class="sticky bottom-[57px] flex gap-2 border-t bg-white p-4">
-      <input type="text" class="w-full rounded border px-4 text-xl" placeholder="Activity name">
+    <form @submit.prevent="emit('createActivity', newActivity)" class="sticky bottom-[57px] flex gap-2 border-t bg-white p-4">
+      <input
+          type="text"
+          class="w-full rounded border px-4 text-xl"
+          :value="newActivity"
+          @input="newActivity = $event.target.value"
+          placeholder="Activity name"
+      >
       <BaseButton>
         <PlusIcon class="h-8"/>
       </BaseButton>
