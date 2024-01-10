5c27ce08c92fff05ed53aca91fee42b98a1c35a4
Alexey <seleznev.trade@gmail.com>
2024-01-10 14:21:03 +0300

    33

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,7 +1,8 @@
 <script>
-import {BUTTON_TYPE_DANGER, BUTTON_TYPE_NEUTRAL} from "@/constants";
+import {BUTTON_TYPE_DANGER, BUTTON_TYPE_NEUTRAL, BUTTON_TYPE_PRIMARY} from "@/constants";
 
 const typeClasses = {
+  [BUTTON_TYPE_PRIMARY]: 'bg-purple-500 enabled:hover:bg-purple-600 text-white ',
   [BUTTON_TYPE_DANGER]: 'bg-red-500 enabled:hover:bg-red-600 text-white ',
   [BUTTON_TYPE_NEUTRAL]: 'bg-gray-100 enabled:hover:bg-gray-200 ',
 }
@@ -11,10 +12,11 @@ const typeClasses = {
 <script setup>
 
 import { isButtonTypeValid } from "@/validators";
+import { BUTTON_TYPE_PRIMARY } from "@/constants";
 
 defineProps({
     type: {
-      default: BUTTON_TYPE_NEUTRAL,
+      default: BUTTON_TYPE_PRIMARY,
       type: String,
       validator: isButtonTypeValid
     }
--- src/components/BaseSelect.vue
+++ src/components/BaseSelect.vue
@@ -1,6 +1,7 @@
 <script setup>
 import BaseButton from "@/components/BaseButton.vue";
 import { XMarkIcon } from "@heroicons/vue/24/outline";
+import { BUTTON_TYPE_NEUTRAL } from "@/constants";
 import {isNumberOrNull, isUndefinedOrNull, validateSelectOptions} from "@/validators";
 import {computed} from "vue";
 
@@ -28,7 +29,7 @@ const isNotSelected = computed(() => isUndefinedOrNull(props.selected))
 
 <template>
   <div class="flex gap-2">
-    <BaseButton @click="emit('select', null)">
+    <BaseButton :type="BUTTON_TYPE_NEUTRAL" @click="emit('select', null)">
       <XMarkIcon class="h-8"/>
     </BaseButton>
     <select
