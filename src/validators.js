77cec1e0df466cbaa44ed7993ad0269ef3324db4
Alexey <seleznev.trade@gmail.com>
2024-01-11 11:55:17 +0300

    40

--- src/validators.js
+++ src/validators.js
@@ -17,8 +17,12 @@ export function validateActivities(activities) {
     return activities.every(isActivityValid)
 }
 
-export function isActivityValid(activity) {
-    return isNotEmptyString(activity)
+export function isActivityValid({ id, name, secondsToComplete }) {
+    return [
+        isNotEmptyString(id),
+        isNotEmptyString(name),
+        isNumber(secondsToComplete)
+    ].every(Boolean)
 }
 
 export function isHourValid(hour) {
@@ -30,7 +34,7 @@ export function validateSelectOptions(options) {
 }
 
 function iSelectOptionValid({ value, label }) {
-    return isNumber(value) && isNotEmptyString(label)
+    return (isNumber(value) || isNotEmptyString(value)) && isNotEmptyString(label)
 }
 
 export function validateTimelineItems(timelineItems) {
